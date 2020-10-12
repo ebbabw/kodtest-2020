223ae7894dada06988eb03c57dfee5832a60ffef
ebbabw <ebbabergholtz@gmail.com>
2020-10-12 23:00:16 +0200

    added more

--- src/pages/Application.js
+++ src/pages/Application.js
@@ -2,23 +2,34 @@ import React from 'react'
 
 import '../css/App.css'
 
+
 export const Application = () => {
-  return (
+
+    return (
     <div>
 
 
         <section>
-
+        
         <div className="section-wrapper" >
 
         <div className="banner">
-        <p></p>
+        <div className="banner-title">
+        <p>Veckans pris!</p>
+        </div>
         <img className="img-1" src='/assets/maner.png' alt='maner'/>
         </div>
         
         <div className="product">
         <div className="product-wrapper">
         <img className="img-2" src='/assets/blabar.png' alt='blabar'/>
+        </div>
+        </div>
+
+        <div className="offer">
+        <img className="img-3" src='/assets/splash.svg' alt='offer' />
+        <div className="offer-title">
+        <p>29</p>
         </div>
         </div>
         
